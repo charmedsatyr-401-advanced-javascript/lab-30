cd778eb6b1bae33b3818b39e3d9a222d25a4b5a5
Joseph <z@charmed.tech>
2019-04-28 11:44:30 -0700

    Clear create-react-app boilerplate and add base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <header>
+        <h1>City Explorer</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Enter a location below to learn about the weather, events, restaurants, movies filmed
+          there, and more!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
+      <main>
+        <form id="url-form">
+          <label>
+            Enter the URL to your deployed back end, making sure to remove the trailing forward
+            slash
+          </label>
+          <input type="text" id="back-end-url" />
+        </form>
+        <form id="search-form" class="hide">
+          <label for="search">Search for a location</label>
+          <input type="text" name="search" id="input-search" placeholder="Enter a location here" />
+          <button type="submit">Explore!</button>
+        </form>
+        <img id="map" class="hide" src="" alt="Map of search query" />
+        <h2 class="query-placeholder">query</h2>
+        {/* Section for error message */}
+        <section class="error-container" />
+
+        <div class="column-container hide">
+          {/* Section for weather data */}
+          <section>
+            <h3>Results from the Dark Sky API</h3>
+            <ul class="weather-results" />
+          </section>
+
+          {/* Section for Yelp data */}
+          <section class="yelp-container">
+            <h3>Results from the Yelp API</h3>
+            <ul class="yelp-results" />
+          </section>
+
+          {/* Section for Meetup data */}
+          <section>
+            <h3>Results from the Meetup API</h3>
+            <ul class="meetups-results" />
+          </section>
+
+          {/* Section for movie data */}
+          <section class="movie-container">
+            <h3>Results from The Movie DB API</h3>
+            <ul class="movies-results" />
+          </section>
+
+          {/* Section for trails data */}
+          <section>
+            <h3>Results from the Hiking Project API</h3>
+            <ul class="trails-results" />
+          </section>
+        </div>
+      </main>
+      <footer>DONE</footer>
+    </>
   );
 }
 
