994940b2a0582854111aecdca21788c19109a364
Joseph <z@charmed.tech>
2019-04-28 20:24:27 -0700

    Add spacing to match original

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Form = props => (
   <form id="search-form">
-    <label htmlFor="search">Search for a location</label>
-    <input type="text" name="search" id="input-search" placeholder="Enter a location here" />
+    <label htmlFor="search">Search for a location</label>{' '}
+    <input type="text" name="search" id="input-search" placeholder="Enter a location here" />{' '}
     <button type="submit" onClick={props.handleSubmit}>
       Explore!
     </button>
